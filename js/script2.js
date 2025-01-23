dd14a14c91f270d0df706ada1131dd3d78e09946
abel hv <115476506+abelHV@users.noreply.github.com>
2025-01-23 19:58:23 +0100

    registro

---
+++ js/script2.js
@@ -0,0 +1,44 @@
+const cardImages = [
+  "../img/reverseMLP.png",
+  "../img/reversePKMN.png",
+  "../img/reverseOP.png",
+  "../img/reverseYu.png",
+  "../img/reverseMTG.png"
+];
+
+const createCard = () => {
+  const card = document.createElement("div");
+  card.className = "falling-card";
+
+  // Posiciones iniciales y finales aleatorias
+  const startX = Math.random();
+  const endX = Math.random();
+
+  card.style.setProperty("--start-x", startX);
+  card.style.setProperty("--end-x", endX);
+
+  // Asegura que la carta empiece fuera de la pantalla
+  card.style.top = "-150px";
+  card.style.left = `calc(100vw * ${startX})`;
+
+  // Duración y retraso aleatorios
+  card.style.animationDuration = `${Math.random() * 5 + 5}s`;
+  card.style.animationDelay = `${Math.random() * 2}s`;
+
+  // Imagen aleatoria
+  const randomCard = cardImages[Math.floor(Math.random() * cardImages.length)];
+  console.log(randomCard);
+  card.style.backgroundImage = `url(${randomCard})`;
+
+  // Añade la carta al contenedor
+  const container = document.querySelector(".animation-card");
+  container.appendChild(card);
+
+  // Elimina la carta al terminar la animación
+  card.addEventListener("animationend", () => {
+    card.remove();
+  });
+};
+
+// Crea una nueva carta cada 3 segundos
+setInterval(createCard, 3000);
